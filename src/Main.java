a1a64fb7fcc8c81ca1f658eff6ac0655393ce4e5
mac <diushalieva.nurzada@gmail.com>
2023-03-28 17:39:26 +0200

    "delete .idea"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+
+
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> input = new ArrayList<Integer>();
+
+        input.addAll(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+
+        List<Integer> filteredInput = new ArrayList<Integer>();
+        for ( int num : input){
+           if(num > 0 ){
+               filteredInput.add(num);
+           }
+       }
+
+        List<Integer> filteredEvenInput = new ArrayList<Integer>();
+        for ( int num : filteredInput){
+            if(num % 2 == 0 ){
+                filteredEvenInput.add(num);
+            }
+        }
+        Collections.sort(filteredEvenInput);
+
+        System.out.println("Original List: " + input);
+        System.out.println("Filtered List: " + filteredInput);
+        System.out.println("Filtered Even List in Ascending order:" + filteredEvenInput);
+
+
+        List<Integer> filteredByApi = StreamMain.filterInputData(input);
+        System.out.println( filteredByApi);
+
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class StreamMain {
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class StreamMain {
+
+    public static List<Integer> filterInputData(List<Integer> input) {
+        input.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+        return null;
+    }
+
 }
+
+
